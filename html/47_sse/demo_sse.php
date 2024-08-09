53c47cab8956b9677abd342f38632c0d8fda8b51
dex <khanhld.developer@gmail.com>
2024-08-09 17:13:02 +0700

    47_sse

---
+++ html/47_sse/demo_sse.php
@@ -0,0 +1,11 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+
+$time = date('r');
+echo "data: The server time is: {$time}\n\n";
+flush();
+?>
